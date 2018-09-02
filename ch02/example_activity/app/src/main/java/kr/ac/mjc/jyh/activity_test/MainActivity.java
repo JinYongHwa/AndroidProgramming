39b63d85c7715527de52ed5984980a9fa293e8b9
unknown <jinyonghwa@naver.com>
2018-09-02 15:18:21 +0900

    example

--- ch02/example_activity/app/src/main/java/kr/ac/mjc/jyh/activity_test/MainActivity.java
+++ ch02/example_activity/app/src/main/java/kr/ac/mjc/jyh/activity_test/MainActivity.java
@@ -1,6 +1,7 @@
 package kr.ac.mjc.jyh.activity_test;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +16,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button newActivityBtn=findViewById(R.id.new_activity_btn);
+        Button callBtn=findViewById(R.id.call_btn);
+
+        callBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Intent.ACTION_DIAL, Uri.parse("tel:01029730833"));
+                startActivity(intent);
+            }
+        });
+
         newActivityBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
