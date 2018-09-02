fdd2181d060e253a3ba95983877823dcf1168273
unknown <jinyonghwa@naver.com>
2018-09-02 13:48:48 +0900

    1강 예제 및 PDf 업로드

---
+++ ch01/example/app/src/main/java/kr/ac/mjc/jyh/example/MainActivity.java
@@ -0,0 +1,29 @@
+package kr.ac.mjc.jyh.example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frame);
+//        LinearLayout mainLayout = new LinearLayout(this);
+//        mainLayout.setOrientation(LinearLayout.VERTICAL);
+//
+//        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+//                LinearLayout.LayoutParams.MATCH_PARENT,
+//                LinearLayout.LayoutParams.WRAP_CONTENT);
+//
+//        Button button1 = new Button(this);
+//        button1.setText("Button1");
+//        button1.setLayoutParams(params);
+//        mainLayout.addView(button1);
+//
+//        setContentView(mainLayout);
+
+    }
+}
