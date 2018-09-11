e0dc484ab5b90e775eb99ed1b3614b59c44efbe7
unknown <jinyonghwa@naver.com>
2018-09-11 23:36:04 +0900

    ch03.pdf add

---
+++ ch03/listview/app/src/main/java/kr/ac/mjc/jyh/listview/MainActivity.java
@@ -0,0 +1,21 @@
+package kr.ac.mjc.jyh.listview;
+
+import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
