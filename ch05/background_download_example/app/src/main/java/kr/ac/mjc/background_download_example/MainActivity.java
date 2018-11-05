356f1c1da54aceff9eeb514c5b1187c98feb1bb1
unknown <jinyonghwa@naver.com>
2018-11-05 19:24:41 +0900

    add test.txt

---
+++ ch05/background_download_example/app/src/main/java/kr/ac/mjc/background_download_example/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.ac.mjc.background_download_example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
