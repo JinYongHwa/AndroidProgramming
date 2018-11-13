b3258cba76a53e333b2a6d4434a3d26d32258dbe
unknown <jinyonghwa@naver.com>
2018-11-13 11:41:09 +0900

    add ch6

--- ch06/config_example/app/src/main/java/kr/ac/mjc/config_example/MainActivity.java
+++ ch06/config_example/app/src/main/java/kr/ac/mjc/config_example/MainActivity.java
@@ -1,15 +1,23 @@
 package kr.ac.mjc.config_example;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.MediaPlayer;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,12 +25,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button settingBtn=findViewById(R.id.setting_btn);
-        settingBtn.setOnClickListener(new View.OnClickListener() {
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setTitle(R.string.main_title);
+
+        Button alarmBtn=findViewById(R.id.alarm_btn);
+        alarmBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,SettingActivity.class);
-                startActivity(intent);
+                SharedPreferences pref= PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+                boolean messageAlarm=pref.getBoolean("message_alarm",false);
+                boolean alarm=pref.getBoolean("alarm",false);
+                String alarmSound=pref.getString("alarm_sound","오바마카카오톡");
+                if(messageAlarm&&alarm){
+                    String fileName="";
+                    MediaPlayer mp = null;
+                    switch (alarmSound){
+                        case "오바마카카오톡":
+                            mp = MediaPlayer.create(MainActivity.this,R.raw.kakao_obama);
+                            break;
+                        case "카카오톡":
+                            mp = MediaPlayer.create(MainActivity.this,R.raw.kakaotalk);
+                            break;
+                        case "카톡":
+                            mp = MediaPlayer.create(MainActivity.this,R.raw.katok);
+                            break;
+                        case "카톡왔숑":
+                            mp = MediaPlayer.create(MainActivity.this,R.raw.katok2);
+                            break;
+                    }
+                    mp.start();
+                    Toast.makeText(MainActivity.this,alarmSound,Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
     }
@@ -31,5 +65,14 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu,menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.setting){
+            Intent intent=new Intent(MainActivity.this,SettingActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- ch06/config_example/app/src/main/java/kr/ac/mjc/config_example/SettingFragment.java
+++ ch06/config_example/app/src/main/java/kr/ac/mjc/config_example/SettingFragment.java
@@ -2,19 +2,62 @@ package kr.ac.mjc.config_example;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.preference.SwitchPreference;
 import android.support.annotation.Nullable;
 
-public class SettingFragment extends PreferenceFragment {
+public class SettingFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
 
+    SwitchPreference messageAlarmSwitch;
+    SwitchPreference alarmSwitch;
+    SwitchPreference vibrateSwitch;
+    ListPreference alarmSoundList;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.setting);
 
         SharedPreferences pref= PreferenceManager.getDefaultSharedPreferences(getActivity());
-        findPreference("message_alarm");
+        messageAlarmSwitch= (SwitchPreference) findPreference("message_alarm");
+        alarmSwitch= (SwitchPreference) findPreference("alarm");
+        vibrateSwitch= (SwitchPreference) findPreference("vibrate");
+        alarmSoundList= (ListPreference) findPreference("alarm_sound");
+        pref.registerOnSharedPreferenceChangeListener(this);
 
+        boolean messageAlarm=pref.getBoolean("message_alarm",false);
+        alarmSoundList.setSummary(pref.getString("alarm_sound", ""));
+        if(messageAlarm){
+            alarmSwitch.setEnabled(true);
+            vibrateSwitch.setEnabled(true);
+            alarmSoundList.setEnabled(true);
+        }
+        else{
+            alarmSwitch.setEnabled(false);
+            vibrateSwitch.setEnabled(false);
+            alarmSoundList.setEnabled(false);
+        }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+
+        if(key.equals("alarm_sound")){
+            alarmSoundList.setSummary(sharedPreferences.getString("alarm_sound", ""));
+        }
+        if(key.equals("message_alarm")){
+            boolean messageAlarm=sharedPreferences.getBoolean("message_alarm",false);
+            if(messageAlarm){
+                alarmSwitch.setEnabled(true);
+                vibrateSwitch.setEnabled(true);
+                alarmSoundList.setEnabled(true);
+            }
+            else{
+                alarmSwitch.setEnabled(false);
+                vibrateSwitch.setEnabled(false);
+                alarmSoundList.setEnabled(false);
+            }
+        }
     }
 }
