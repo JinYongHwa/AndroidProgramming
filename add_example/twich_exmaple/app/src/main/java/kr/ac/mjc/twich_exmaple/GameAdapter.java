8de0b322771b19b35a6de6bc99f063920752d080
unknown <jinyonghwa@naver.com>
2018-12-05 10:44:48 +0900

    add

---
+++ add_example/twich_exmaple/app/src/main/java/kr/ac/mjc/twich_exmaple/GameAdapter.java
@@ -0,0 +1,48 @@
+package kr.ac.mjc.twich_exmaple;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+
+public class GameAdapter extends BaseAdapter {
+    Context mContext;
+    ArrayList<Top> mGameList;
+
+    public GameAdapter(Context context, ArrayList<Top> gameList){
+        this.mContext=context;
+        this.mGameList=gameList;
+    }
+
+    @Override
+    public int getCount() {
+        return mGameList.size();
+    }
+
+    @Override
+    public Top getItem(int position) {
+        return mGameList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        GameItemLayout itemLayout;
+        if(convertView==null){
+            itemLayout=new GameItemLayout(mContext);
+        }
+        else{
+            itemLayout= (GameItemLayout) convertView;
+        }
+        Top top=getItem(position);
+        itemLayout.setTop(top);
+
+        return itemLayout;
+    }
+}
---
+++ add_example/twich_exmaple/app/src/main/java/kr/ac/mjc/twich_exmaple/Image.java
@@ -0,0 +1,40 @@
+package kr.ac.mjc.twich_exmaple;
+
+public class Image {
+    private String large;
+    private String medium;
+    private String small;
+    private String template;
+
+    public String getLarge() {
+        return large;
+    }
+
+    public void setLarge(String large) {
+        this.large = large;
+    }
+
+    public String getMedium() {
+        return medium;
+    }
+
+    public void setMedium(String medium) {
+        this.medium = medium;
+    }
+
+    public String getSmall() {
+        return small;
+    }
+
+    public void setSmall(String small) {
+        this.small = small;
+    }
+
+    public String getTemplate() {
+        return template;
+    }
+
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+}
